2e45411cef75c44fb1c8b5c1969db8d9e1bf8087
Yiğithan Karabulut <yigithannkarabulutt@gmail.com>
2024-07-02 02:19:47 +0300

    add id field from necessary function params.

--- store/store.go
+++ store/store.go
@@ -40,8 +40,8 @@ func WithPathTransformFunc(f PathTransformFunc) Option {
 // NewStore creates a new Store with the given options.
 func NewStore(opts ...Option) *Store {
 	s := &Store{
-		PathTransformFunc: DefaultPathTransformFunc,
 		Root:              defaultRootFolderName,
+		PathTransformFunc: DefaultPathTransformFunc,
 	}
 	for _, opt := range opts {
 		opt(s)
@@ -55,9 +55,9 @@ func (s *Store) Clear() error {
 }
 
 // Has checks if a key exists in the storage.
-func (s *Store) Has(key string) bool {
+func (s *Store) Has(id, key string) bool {
 	pathKey := s.PathTransformFunc(key)
-	pathNameWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.FullPath())
+	pathNameWithRoot := fmt.Sprintf("%s/%s/%s", s.Root, id, pathKey.FullPath())
 
 	_, err := os.Stat(pathNameWithRoot)
 
@@ -65,29 +65,61 @@ func (s *Store) Has(key string) bool {
 }
 
 // Delete deletes a key from the storage.
-func (s *Store) Delete(key string) error {
+func (s *Store) Delete(id, key string) error {
 	pathKey := s.PathTransformFunc(key)
 
 	defer func() {
 		log.Printf("deleted [%s] from disk\n", pathKey.FullPath())
 	}()
 
-	pathNameWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.FirstPathName())
+	pathNameWithRoot := fmt.Sprintf("%s/%s/%s", s.Root, id, pathKey.FirstPathName())
 	return os.RemoveAll(pathNameWithRoot)
 }
 
 // Write writes a key to the storage.
-func (s *Store) Write(key string, r io.Reader) (int64, error) {
-	return s.writeStream(key, r)
+func (s *Store) Write(id, key string, r io.Reader) (int64, error) {
+	return s.writeStream(id, key, r)
 }
 
-func (s *Store) Read(key string) (int64, io.Reader, error) {
-	return s.readStream(key)
+// WriteDecrypt writes a key to the storage with decryption. It uses the given encryption key to decrypt the data.
+func (s *Store) WriteDecrypt(encryptKey []byte, id, key string, r io.Reader) (int64, error) {
+	f, err := s.openFileForWriting(id, key)
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := crypto.CopyDecrypt(encryptKey, r, f)
+	return int64(n), err
 }
 
-func (s *Store) readStream(key string) (int64, io.ReadCloser, error) {
+func (s *Store) openFileForWriting(id, key string) (*os.File, error) {
 	pathKey := s.PathTransformFunc(key)
-	pathKeyWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.FullPath())
+	pathNameWithRoot := fmt.Sprintf("%s/%s/%s", s.Root, id, pathKey.PathName)
+	if err := os.MkdirAll(pathNameWithRoot, os.ModePerm); err != nil { //nolint:gosec
+		return nil, err
+	}
+
+	fullPathWithRoot := fmt.Sprintf("%s/%s/%s", s.Root, id, pathKey.FullPath())
+
+	return os.Create(fullPathWithRoot) //nolint:gosec
+}
+
+func (s *Store) writeStream(id, key string, r io.Reader) (int64, error) {
+	f, err := s.openFileForWriting(id, key)
+	if err != nil {
+		return 0, err
+	}
+
+	return io.Copy(f, r)
+}
+
+func (s *Store) Read(id, key string) (int64, io.Reader, error) {
+	return s.readStream(id, key)
+}
+
+func (s *Store) readStream(id, key string) (int64, io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	pathKeyWithRoot := fmt.Sprintf("%s/%s/%s", s.Root, id, pathKey.FullPath())
 
 	file, err := os.Open(pathKeyWithRoot) //nolint:gosec
 	if err != nil {
@@ -100,40 +132,4 @@ func (s *Store) readStream(key string) (int64, io.ReadCloser, error) {
 	}
 
 	return fi.Size(), file, nil
-}
-
-// WriteDecrypt writes a key to the storage with decryption. It uses the given encryption key to decrypt the data.
-func (s *Store) WriteDecrypt(encryptKey []byte, key string, r io.Reader) (int64, error) {
-	pathKey := s.PathTransformFunc(key)
-	pathNameWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.PathName)
-	if err := os.MkdirAll(pathNameWithRoot, os.ModePerm); err != nil { //nolint:gosec
-		return 0, err
-	}
-
-	fullPathWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.FullPath())
-
-	f, err := os.Create(fullPathWithRoot) //nolint:gosec
-	if err != nil {
-		return 0, err
-	}
-
-	n, err := crypto.CopyDecrypt(encryptKey, r, f)
-	return int64(n), err
-}
-
-func (s *Store) writeStream(key string, r io.Reader) (int64, error) {
-	pathKey := s.PathTransformFunc(key)
-	pathNameWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.PathName)
-	if err := os.MkdirAll(pathNameWithRoot, os.ModePerm); err != nil { //nolint:gosec
-		return 0, err
-	}
-
-	fullPathWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.FullPath())
-
-	f, err := os.Create(fullPathWithRoot) //nolint:gosec
-	if err != nil {
-		return 0, err
-	}
-
-	return io.Copy(f, r)
 }
